0070d0a5a41a917ada88ced35547f6de832eeadb
User-r <rwagner6282@yahoo.com>
2023-03-10 10:26:22 -0600

    Sunrise Update

---
+++ README.md
@@ -0,0 +1,71 @@
+# Middle East / North Africa Countries Overview Application
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Installation](#installation)
+- [Updates](#updates)
+- [Technologies](#technologies)
+- [License](#license)
+- [Acknowledgments](#acknowledgments)
+
+## Introduction
+
+Welcome to the Middle East / North Africa Countries Overview application (MENACO). Currently, the goal of this project is to create a simple and user friendly application that will display a general overview of a country's population, majority religion, and GDP.
+
+## Installation
+
+1. Fork this repository
+2. Clone the forked repository
+3. To use live-server, type the following code into your terminal:
+
+- _Please note that this code both installs npm and npm live-server. If you already have both of these on your system, proceed to step 4._
+
+```
+npm install -g npm
+
+npm install -g live-server
+
+```
+
+4. Find the location of the repository on your local device:
+
+```
+cd [Repository location on local device]
+```
+
+5. Type the following in the terminal to begin the application:
+
+```
+live-server
+```
+
+## Updates
+
+### Version 1.0 (Sunrise)
+
+**Sunrise** is the very first user interactive version of the MENACO application. Users must first select a country by clicking on it from a pre-determined list for the information to appear in the **_stats_** section. Currently, all input values for the **Sunrise** version are located in arrays located in _script.js_. Future plans include using an API to create these and more values dynamically. **Please note that currently this ia a desktop-only application.**
+
+Features added include:
+
+- Interactive GUI
+- Navigation bar
+- Reset Button
+- README.md
+
+## Technologies
+
+Project created with:
+
+- HTML
+- CSS
+- JavaScript
+
+## License
+
+Distributed under the MIT License. See `LICENSE.md` for more information.
+
+## Acknowledgments
+
+- [CIA World Factbook](https://www.cia.gov/the-world-factbook/)
+- [The Complete JavaScript Course 2023](https://www.udemy.com/course/the-complete-javascript-course/) by Jonas Schmedtmann
--- script.js
+++ script.js
@@ -8,31 +8,33 @@ const countries = [
   "Iraq",
   "Israel",
   "Jordan",
+  "Lebanon",
   "Libya",
   "Oman",
+  "Qatar",
   "Saudi Arabia",
   "Sudan",
   "Syria",
   "Turkey",
-  "United Ara Emirates",
-  "West Bank",
+  "United Arab Emirates",
 ];
 const population = [
   "38,346,720",
   "1,540,558",
   "107,770,524",
-  "86,758, 304",
+  "86,758,304",
   "40,462,701",
   "8,914,855",
   "10,998,531",
+  "5,331,203",
   "7,137,931",
   "3,764,348",
+  "2,532,104",
   "35,354,380",
   "47,958,856",
   "21,563,800",
   "83,047,706",
   "9,915,803",
-  "3,000,021",
 ];
 const religion = [
   "Sunni",
@@ -42,14 +44,15 @@ const religion = [
   "Shia",
   "Jewish",
   "Sunni",
+  "Muslim",
   "Sunni",
+  "Muslim",
   "Muslim",
   "Sunni",
   "Sunni",
   "Sunni",
   "Sunni",
   "Muslim",
-  "Sunni",
 ];
 const gdp = [
   "60.803 billion",
@@ -59,39 +62,62 @@ const gdp = [
   "390.165 billion",
   "393.861 billion",
   "102.822 billion",
+  "72.577 billion",
   "147.942 billion",
   "155.028 billion",
+  "249.639 billion",
   "1.594 trillion",
   "168.98 billion",
   "50.28 billion",
   "2.668 trillion",
   "653.067 billion",
-  "27.779 billion",
 ];
 
-const countryFacts = (country) => {
-  if (!countries.includes(country)) {
-    prompt("No match found. Please try another country.");
-    enterCountry();
-  } else {
-    const x = countries.indexOf(country);
-    return `
-  ${country}
-  population: ${population[x]}
-  religion: ${religion[x]}
-  GDP: $${gdp[x]}
-  `;
-  }
+//Variables to work with created
+const country = document.querySelector(`.state`);
+const populationStats = document.querySelector(`.pop-stat`);
+const religionStats = document.querySelector(`.rel-stat`);
+const gdpStats = document.querySelector(`.gdp-stat`);
+
+const resetbtn = document.querySelector(`.reset`);
+const btnState = document.querySelectorAll(`.state-btn`);
+
+// Select year variable and change innerHTML to current year
+const year = document.querySelector(`.year`);
+
+year.innerHTML = new Date().getFullYear();
+
+// Get clicked country id and return innerHTML of country as state
+const pickCountry = (btn) => {
+  const stateID = btn.id;
+  const state = document.getElementById(stateID).innerHTML;
+  return state;
 };
 
-function enterCountry() {
-  const choice = prompt("Type a country from the Middle East: ")
-    .toLowerCase()
-    .split("");
-  const firstLetter = choice[0].toUpperCase();
-  const otherLetters = choice.slice(1).join("");
-  return firstLetter + otherLetters;
+// Get country index and use to change content of pop, religion, and gdp elements on page
+const countryFacts = (state) => {
+  const i = countries.indexOf(state);
+  country.textContent = countries[i];
+  populationStats.textContent = population[i];
+  religionStats.textContent = religion[i];
+  gdpStats.textContent = gdp[i];
+};
+
+// Reset all value to default
+const reset = () => {
+  country.textContent = `Country`;
+  populationStats.textContent = `0`;
+  religionStats.textContent = `Atheist`;
+  gdpStats.textContent = `0`;
+};
+
+//Iterate over every btnState and add pickCountry function to each one
+for (let i = 0; i < btnState.length; i++) {
+  btnState[i].addEventListener("click", function () {
+    const state = pickCountry(this);
+    countryFacts(state);
+  });
 }
 
-const facts = countryFacts(enterCountry());
-console.log(facts);
+// Add reset funtion to resetbtn
+resetbtn.addEventListener(`click`, reset);
